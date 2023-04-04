92dc8b108c1d457e18c53f6a901aad2114219fab
mkankit07 <mkankit189@gmail.com>
2023-04-04 23:33:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />}/>
+            <Route path="/contact" element={<Contact />}/>
+            <Route path="/about" element={<About />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
